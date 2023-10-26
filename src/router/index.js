afc157327eccc3e2366a99b6398124b7502f8bbf
jdsteege <jdsteege@gmail.com>
2023-10-26 09:40:54 -0500

    Scaffolding landing page

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    component: () => import("@/pages/LandingPage.vue"),
+    component: () => import("@/views/LandingPage.vue"),
     children: [
       { path: "/", redirect: "/main-bucket-list" },
       {
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,8 +1,39 @@
-
 <template>
-  <HelloWorld />
+  <v-layout>
+    <v-app-bar color="primary">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Photos</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
+
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg" title="John Leider"></v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+
+  </v-layout>
 </template>
 
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import { ref } from 'vue';
+
+var drawer = ref(false);
 </script>
